91e3cd77e4f006df0f848efe50db8030efac8544
Dayanithi030208 <dayanithiganapathi@gmail.com>
2026-03-02 10:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (36.83%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -7,25 +7,35 @@ class Solution {
         }
         for (int i = 0 ; i < m ; i++){
             if(visited[i] == -1){
-                if(!colorBFS (graph , i , visited)) return false ;
+                if(!colorDFS (graph , i , visited,0)) return false ;
             }
         }
         return true;
     }
-    private boolean colorBFS (int[][] graph , int start , int[] visited){
-        Queue<Integer> queue = new LinkedList<>() ;
-        queue.add(start) ;
-        visited[start] = 1;
-        while ( !queue.isEmpty() ){
-            int node = queue.peek();
-            queue.poll();
-            for (int adj : graph[node]){
-                if(visited[adj] == -1){
-                    visited[adj] = 1 - visited[node];
-                    queue.add(adj);
-                }
-                else if (visited[node] == visited[adj]) return false;
+    // private boolean colorBFS (int[][] graph , int start , int[] visited){
+    //     Queue<Integer> queue = new LinkedList<>() ;
+    //     queue.add(start) ;
+    //     visited[start] = 1;
+    //     while ( !queue.isEmpty() ){
+    //         int node = queue.peek();
+    //         queue.poll();
+    //         for (int adj : graph[node]){
+    //             if(visited[adj] == -1){
+    //                 visited[adj] = 1 - visited[node];
+    //                 queue.add(adj);
+    //             }
+    //             else if (visited[node] == visited[adj]) return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    private boolean colorDFS (int[][] graph , int start , int[] visited , int color){
+        visited[start] = color;
+        for (int adj : graph[start]){
+            if(visited[adj] == -1){
+                if(!colorDFS(graph ,adj ,visited ,1 - color)) return false;
             }
+            else if (visited[adj] == color) return false;
         }
         return true;
     }
