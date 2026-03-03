f54db4ed155d07bdeac2fdf8240105a3e90c7adf
Dayanithi030208 <dayanithiganapathi@gmail.com>
2026-03-03 16:06:20 +0530

    Time: 3 ms (85.85%) | Memory: 47.5 MB (86.10%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -2,7 +2,6 @@ class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
         int[] leftcheck = new int[n];
-        int[] rightcheck = new int[n];
         leftcheck[0] = 1;
         int c = 1;
         for(int i = 1 ; i < n ; i++){
@@ -16,20 +15,18 @@ class Solution {
             }
         }
         c = 1;
+        int chocolates = 0 ;
         for(int i = n - 2 ; i >= 0 ; i--){
             if(ratings[i] > ratings[i + 1]){
                 c++;
-                rightcheck[i] = c;
+                chocolates += Math.max(leftcheck[i] ,c);
             }
             else{
                 c = 1;
-                rightcheck[i] = c;
+                chocolates += Math.max(leftcheck[i] ,c);
             }
         }
-        int chocolates = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            chocolates += Math.max(leftcheck[i] , rightcheck[i]);
-        }
+        chocolates += Math.max(leftcheck[n - 1],1);
         return chocolates;
     }
 }
