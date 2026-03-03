3c7765e96eb3a486107b15b6fa04e565264df8a0
Dayanithi030208 <dayanithiganapathi@gmail.com>
2026-03-03 17:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (35.44%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,24 +1,44 @@
 class Solution {
     public boolean checkValidString(String s) {
-        if (s.isEmpty()) return false;
+        // if (s.isEmpty()) return false;
+        // int n = s.length();
+        // Boolean[][] memo = new Boolean[n][n];
+        // return checkingValidString(s , 0 , 0,memo);
+        int min = 0;
+        int max = 0;
         int n = s.length();
-        Boolean[][] memo = new Boolean[n][n];
-        return checkingValidString(s , 0 , 0,memo);
+        for (int i = 0 ; i < n ; i++){
+            if(s.charAt(i) == '('){
+                min++;
+                max++;
+            }
+            else if(s.charAt(i) == ')'){
+                min--;
+                max--;
+            }
+            else{
+                min--;
+                max++;
+            }
+            if(min < 0) min = 0;
+            if(max < 0) return false;
+        }
+        return min == 0;
     }
-    private boolean checkingValidString(String s , int index , int count , Boolean[][] memo){
-        int n = s.length();
-        if(count < 0) return false;
-        if(index == n) return count == 0;
-        if(memo[index][count] != null) return memo[index][count];
-        if(s.charAt(index) == '(') {
-            memo[index][count] = checkingValidString(s,index + 1 , count + 1,memo);
-        }
-        else if(s.charAt(index) == ')') {
-            memo[index][count] = checkingValidString(s,index + 1 , count - 1,memo);
-        }
-        else{
-            memo[index][count] = checkingValidString(s, index + 1, count + 1,memo) || checkingValidString(s, index + 1 , count - 1,memo) || checkingValidString(s, index + 1, count,memo);
-        }
-        return memo[index][count];
-    }
+    // private boolean checkingValidString(String s , int index , int count , Boolean[][] memo){
+        // int n = s.length();
+        // if(count < 0) return false;
+        // if(index == n) return count == 0;
+        // if(memo[index][count] != null) return memo[index][count];
+        // if(s.charAt(index) == '(') {
+        //     memo[index][count] = checkingValidString(s,index + 1 , count + 1,memo);
+        // }
+        // else if(s.charAt(index) == ')') {
+        //     memo[index][count] = checkingValidString(s,index + 1 , count - 1,memo);
+        // }
+        // else{
+        //     memo[index][count] = checkingValidString(s, index + 1, count + 1,memo) || checkingValidString(s, index + 1 , count - 1,memo) || checkingValidString(s, index + 1, count,memo);
+        // }
+        // return memo[index][count];
+    // }
 }
