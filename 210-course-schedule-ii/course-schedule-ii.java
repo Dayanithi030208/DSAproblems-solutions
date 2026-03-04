d687bd63aab284238b0ac53f3e095eef01b1ca7f
Dayanithi030208 <dayanithiganapathi@gmail.com>
2026-03-04 15:49:43 +0530

    Time: 8 ms (20.86%) | Memory: 47.4 MB (30.81%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            adj.add(new ArrayList<>());
+        }
+        for (int[] it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj.get(u).add(v);
+        }
+        int[] indegree = new int[numCourses];
+        for(int i = 0 ; i < numCourses ; i++){
+            for(Integer it : adj.get(i)){
+            indegree[it]++;
+        }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+        int count = 0;
+        List<Integer> result = new ArrayList<>();
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            result.add(node);
+            count++;
+            for(Integer it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    queue.offer(it);
+                }
+            }
+        }
+        if(count != numCourses) return new int[] {};
+        int[] ans = new int[numCourses];
+        for (int i = 0 ; i < numCourses ; i++){
+            ans[numCourses - i -1] = result.get(i);
+        }
+        return ans;
+    }
+}
