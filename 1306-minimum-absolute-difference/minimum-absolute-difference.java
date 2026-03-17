48ae17df4721dd8797d6511099482b402f929eb9
Dayanithi030208 <dayanithiganapathi@gmail.com>
2026-03-17 20:18:03 +0530

    Time: 21 ms (68.82%) | Memory: 63.8 MB (67.04%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
         int n = arr.length;
-        mergesort(arr , 0 , n - 1);
+        Arrays.sort(arr);
         int minDifference = Integer.MAX_VALUE;
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 1 ; i < n ; i++){
@@ -15,30 +15,5 @@ class Solution {
             }
         }
         return result;
-    }
-    public static void mergesort(int[] nums,int left,int right){
-        int mid=(left+right)/2;
-        if(left<right){
-            mergesort(nums,left,mid);
-            mergesort(nums,mid+1,right);
-            merge(nums,left,mid,right);
-        }
-    }
-    public static void merge(int[] nums,int left, int mid,int right){
-        int[] temp=new int[right-left+1];
-        int i=left,k=0,j=mid+1;
-        while(i<=mid&&j<=right){
-            if(nums[i]<nums[j]){
-                temp[k++]=nums[i++];
-            }
-            else{
-                temp[k++]=nums[j++];
-            }
-        }
-        while(i<=mid) temp[k++]=nums[i++];
-        while(j<=right) temp[k++]=nums[j++];
-        for(int p=0;p<temp.length;p++){
-            nums[left+p]=temp[p];
-        }
     }
 }
